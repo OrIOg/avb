83d2149c409fa0463fee4de53089d75ec80150a3
OrIOg <thibault_poisso@orange.fr>
2020-02-03 17:40:27 +0100

    main

---
+++ app/src/main/java/com/example/avosbarsquickanddirty/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.avosbarsquickanddirty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView1);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
